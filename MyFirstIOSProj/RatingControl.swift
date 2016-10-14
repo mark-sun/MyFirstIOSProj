c9ef2cbf62358f26bc14d34bdd758774cfb870cb
Mark Sun <mark@clari.com>
2016-10-14 02:44:12 -0700

    finished rating part

---
+++ MyFirstIOSProj/RatingControl.swift
@@ -0,0 +1,77 @@
+//
+//  RatingControl.swift
+//  MyFirstIOSProj
+//
+//  Created by Mark Sun on 10/14/16.
+//  Copyright © 2016 Mark Sun. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    
+    
+    // MARK: Properties
+    var rating = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    var ratingButtons = [UIButton]()
+    let spacing = 15
+    let starCount = 5
+    var buttonSize = 0
+    
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let blackStartImage = UIImage(named: "blackstar")
+        let whiteStartImage = UIImage(named: "whitestar")
+        
+        buttonSize = 44
+        
+        for _ in 0..<starCount {
+            let button = UIButton()
+            button.setImage(whiteStartImage, forState: .Normal)
+            button.setImage(blackStartImage, forState: .Selected)
+            button.setImage(blackStartImage, forState: [.Highlighted, .Selected])
+            button.adjustsImageWhenHighlighted = false
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        return CGSize(width: width, height: buttonSize)
+    }
+    
+    override func layoutSubviews() {
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        // Offset each button's origin by the length of the button plus spacing.
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
+            button.frame = buttonFrame
+        }
+        updateButtonSelectionStates()
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
+        rating = ratingButtons.indexOf(button)! + 1
+        
+        updateButtonSelectionStates()
+    }
+    
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerate() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.selected = index < rating
+        }
+    }
+}
--- MyFirstIOSProj/ViewController.swift
+++ MyFirstIOSProj/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        textbox.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: Properties
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var textbox: UITextField!
+    @IBOutlet weak var imageBox: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    // MARK: Actions
+    @IBAction func touchButton(sender: UIButton) {
+        showPhotoLibPicker()
     }
-
+    @IBAction func touchImage(sender: UITapGestureRecognizer) {
+        showPhotoLibPicker()
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(textField: UITextField) {
+        myLabel.text = textField.text?.uppercaseString
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        imageBox.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func showPhotoLibPicker () {
+        // Hide the keyboard.
+        textbox.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
 
 }
 
